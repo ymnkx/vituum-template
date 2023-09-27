9fd00db2ab95e3638f180bc15016e777417e1797
Yamanaka Tomohiro <yamanaka@un-t.com>
2023-09-27 17:13:28 +0900

    ぺージごとのscriptのサンプルを追加
    buildテストのため

--- src/scripts/common.ts
+++ src/scripts/common.ts
@@ -1,6 +1,1 @@
-// import { SampleComponent } from '@/scripts/components/SampleComponent';
-import { SampleComponent } from '@ts/components/SampleComponent';
-
-SampleComponent();
-
 console.log('common.ts');
