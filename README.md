bd80bb7910e28b8f95aaa8cee137db5a72410602
Yamanaka Tomohiro <yamanaka@un-t.com>
2024-05-27 18:34:32 +0900

    パブリックパス設定を追加

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # vituum-template
+
+Vite に Vituum を追加して制作した開発環境のサンプルです。
+
+# public path
+
+Vituum を使いつつ Vite のbase設定（パブリックパス設定）をすると、パスがズレて開発できません。
+開発（development）はデフォルト設定、ビルド（production）では必要なパスを設定できるようにしました。
+パブリックパスは、環境変数（.env、.env.production）の `VITE_BASE_PATH` で指定します。
--- vite.config.js
+++ vite.config.js
@@ -1,48 +1,54 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vituum from 'vituum';
 import nunjucks from '@vituum/vite-plugin-nunjucks';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import process from 'node:process';
 
 import path from 'path';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@/': `${__dirname}/src/`,
-      '@scss/': `${__dirname}/src/styles/`,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    base: env.VITE_BASE_PATH,
+    resolve: {
+      alias: {
+        '@/': `${__dirname}/src/`,
+        '@scss/': `${__dirname}/src/styles/`,
+      },
     },
-  },
-  server: {
-    host: true,
-  },
-  plugins: [
-    vituum({
-      imports: {
-        filenamePattern: {
-          '+.css': [],
-          '+.scss': 'src/styles',
-          '+.js': [],
-          '+.ts': 'src/scripts',
+    server: {
+      host: true,
+    },
+    plugins: [
+      vituum({
+        imports: {
+          filenamePattern: {
+            '+.css': [],
+            '+.scss': 'src/styles',
+            '+.js': [],
+            '+.ts': 'src/scripts',
+          },
+        },
+      }),
+      nunjucks({
+        root: './src',
+      }),
+      tsconfigPaths(),
+    ],
+    build: {
+      emptyOutDir: true,
+      polyfillModulePreload: false,
+      rollupOptions: {
+        output: {
+          assetFileNames: 'assets/css/[name].[ext]',
+          chunkFileNames: 'assets/js/[name].js',
+          entryFileNames: 'assets/js/[name].js',
         },
       },
-    }),
-    nunjucks({
-      root: './src',
-    }),
-    tsconfigPaths(),
-  ],
-  build: {
-    emptyOutDir: true,
-    polyfillModulePreload: false,
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/css/[name].[ext]',
-        chunkFileNames: 'assets/js/[name].js',
-        entryFileNames: 'assets/js/[name].js',
-      },
     },
-  },
+  };
 });
